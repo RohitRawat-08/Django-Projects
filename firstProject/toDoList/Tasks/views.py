c2e87b179862c11594c0085bf2cb17c3d47100b9
Rohit <rsrawat2929@gmail.com>
2024-08-14 23:32:41 +0530

    added delete and edit buttons

--- firstProject/toDoList/Tasks/views.py
+++ firstProject/toDoList/Tasks/views.py
@@ -17,3 +17,30 @@ def home(request):
     context={'data':data}
 
     return render (request,'index.html',context)
+
+
+
+def edit(request ,id):
+    data=Task.objects.get(id=id)
+
+    if request.method == 'POST':
+        data.Task=request.POST['Task']
+        data.desc=request.POST['desc']
+        data.save()
+        return redirect(home)
+
+    context ={'data':data}
+    return render (request, 'form.html',context)
+
+
+def delete(request ,id):
+    data=Task.objects.get(id=id)
+    
+    if request.method == 'POST':
+        data.delete()
+        return redirect(home)
+
+    return render(request,'delete.html')
+
+
+
