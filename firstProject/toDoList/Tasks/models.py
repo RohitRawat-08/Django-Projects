ed7d8738ed3d0a5e4817f625f843db391026bc92
Rohit <rsrawat2929@gmail.com>
2024-08-14 22:43:53 +0530

    Task & desc added

--- firstProject/toDoList/Tasks/models.py
+++ firstProject/toDoList/Tasks/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Task(models.Model):
+    Task = models.CharField(max_length=30)
+    desc = models.CharField(max_length=70)
--- firstProject/toDoList/Tasks/views.py
+++ firstProject/toDoList/Tasks/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from Tasks.models import Task
+
 
 # Create your views here.
+
+def home(request):
+
+    if request.method == 'POST':
+        Task.objects.create(
+            Task=request.POST['Task'],
+            desc=request.POST['desc']
+        )
+        return redirect(home)
+
+    data=Task.objects.all()
+    context={'data':data}
+
+    return render (request,'index.html',context)
