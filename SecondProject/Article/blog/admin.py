83b90313690bfde661bd699c716e98869c8cb25f
Rohit <rsrawat2929@gmail.com>
2024-08-20 23:48:46 +0530

    add data in dataBase

--- SecondProject/Article/blog/admin.py
+++ SecondProject/Article/blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from blog.models import DataBase
+
 
 # Register your models here.
+
+class DataBaseAdmin(admin.ModelAdmin):
+    list_display=['id','Place','description']
+
+
+admin.site.register(DataBase,DataBaseAdmin)
