a9e847b69a012125fa1d20ac4af0115290b941e7
Rohit <rsrawat2929@gmail.com>
2024-08-20 23:59:42 +0530

    add display page

--- SecondProject/Article/blog/urls.py
+++ SecondProject/Article/blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from blog import views
 
 urlpatterns= [
-    path('',views.home)
+    path('',views.home, name='home'),
+    path('display/',views.display, name='display'),
 ]
--- SecondProject/Article/blog/views.py
+++ SecondProject/Article/blog/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "home.html")
+
+
+def display(request):
+    return render(request,"display.html")
